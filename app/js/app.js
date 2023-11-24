91d169a4fd152fdf52320e81d44242583f4fd8ce
rg27 <gualvezreymar@gmail.com>
2023-11-24 17:46:34 +0400

    Adding Clone button and functionality

--- app/js/app.js
+++ app/js/app.js
@@ -1,17 +1,67 @@
 //https://127.0.0.1:5000/app/widget.html
 //variables
 const inputSearch = document.getElementById("mySearch")
-// Get Entity Data from CRM (Note: this only works within Zoho CRM)
-// ZOHO.embeddedApp.on("PageLoad", entity => {
-//     // This is the information about the current record, if applicable.
-//     let entity_id = entity.EntityId[0];
-//     //Get Record
-//     ZOHO.CRM.API.getRecord({
-//     Entity: "Deals", approved: "both", RecordID:entity_id
-//     })
+let user_id;
+let quote_owner_name;
+let quote_owner_id;
+let quote_owner_email;
+let subject;
+let vat_amt;
+let crm_license_prod;
+let crm_license_desc;
+let crm_jurisdiction;
+let description_info;
+let billing_street;
+let billing_city;
+let billing_state;
+let billing_code;
+let billing_country;
+let prospect_quote_assigned;
+
+//Prospects
+let prospect_id;
  
-  
-// });
+document.getElementById("clone_button_id").style.display = "none";
+// Get Entity Data from CRM (Note: this only works within Zoho CRM)
+ZOHO.embeddedApp.on("PageLoad", entity => {
+    // This is the information about the current record, if applicable.
+    let entity_id = entity.EntityId[0];
+    //Get Record
+    ZOHO.CRM.API.getRecord({
+    Entity: "Quotes", approved: "both", RecordID:entity_id
+    })
+    .then(function(data){
+      const quote_data = data.data
+      console.log(quote_data)
+      quote_data.map( (data)=> {
+         quote_owner_name = data.Owner.name
+         quote_owner_id = data.Owner.id
+         quote_owner_email = data.email
+         subject = data.Subject
+         vat_amt = data.VAT_Amount
+         crm_license_prod = data.CRM_License_Product
+         crm_license_desc = data.CRM_License_Description
+         crm_jurisdiction = data.CRM_Jurisdiction
+         description_info = data.Description
+         billing_street = data.Billing_Street
+         billing_city = data.Billing_City
+         billing_state = data.Billing_State
+         billing_code = data.Billing_Code
+         billing_country = data.Billing_Country
+      });
+  });
+  ZOHO.CRM.CONFIG.getCurrentUser()
+  .then(function(data){
+   console.log("Current User: ")
+	console.log(data);
+   const user_data = data.data
+   user_data.map( (data)=> {
+      console.log(data)
+      prospect_id = data.id;
+     
+   });
+});
+});
 
 // Initialize Widget Connection
 ZOHO.embeddedApp.init();
@@ -24,6 +74,10 @@ function search_prospect()
          let data2 = "";
          datas.map( (data)=> {
             console.log(data)
+            prospect_id = data.id;
+            prospect_name = data.Deal_Name
+            prospect_quote_assigned = data.Quote_Assigned
+
             data2 +=`<div class="container left">
             <div class="content">
             <h3>Prospect Information</h3>
@@ -34,7 +88,7 @@ function search_prospect()
             </div>`
          });
          document.getElementById("timeline").innerHTML = data2
-        //  document.getElementById('clone_id').style.visibility = 'visible';
+         document.getElementById("clone_button_id").style.display = "block";
         
      }).catch(err=>{
         let data3 = "";
@@ -42,10 +96,39 @@ function search_prospect()
         console.log("error 2");
         data3 +=`<div class="container left">
         <div class="content">
-        <h3>Error, no found!</h3>
+        <h3>Error, no prospect found!</h3>
         </div>
         </div>`
         document.getElementById("timeline").innerHTML = data3
-        // document.getElementById('clone_id').style.visibility = 'hidden';
+        document.getElementById("clone_button_id").style.display = "none";
      })
 }
+
+function clone_quote()
+{
+   console.log("Quote Informations")
+   console.log(quote_owner_name)
+   console.log(quote_owner_id)
+   console.log(quote_owner_email)
+   console.log(subject)
+   console.log("Prospect Informations")
+   console.log(prospect_id)
+   console.log(prospect_name)
+   console.log(prospect_quote_assigned)
+   if(prospect_quote_assigned)
+   {
+      alert("Prospect Quote assign is NOT null");
+      
+   }
+   else
+   {
+      var recordData = {
+         "Company": "Zylker",
+         "Last_Name": "Peterson"
+   }
+ ZOHO.CRM.API.insertRecord({Entity:"Quotes",APIData:recordData,Trigger:["workflow"]}).then(function(data){
+    console.log(data);
+    });
+   }
+}
+
